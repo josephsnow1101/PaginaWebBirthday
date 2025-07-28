d9cff5168caabd398a6e32d17cb128f7ae357da2
josephsnow1101 <josephsnowsnow1101@gmail.com>
2025-07-28 15:58:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Escribir aqui el código Javascript
+<script>
+  const btnDescargar = document.querySelector('a[download]');
+  btnDescargar.addEventListener('click', () => {
+    confetti({
+      particleCount: 80,
+      spread: 100,
+      origin: { y: 0.7 }
+    });
+  });
+</script>
